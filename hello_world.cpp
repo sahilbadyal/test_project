0dfd396a1f53b3d42a512b59a21f506796b6c4bf
Sahil Badyal <sbadyals@gmail.com>
2023-09-30 14:50:57 -0400

    fixes the bug in argv parameter

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-int main(int argc, char * argv){
+int main(int argc, char** argv){
     if (argc > 1){
-        cout << "Hello" << argv[1];
+        cout << "Hello " << argv[1] << endl;
     }else{
-        cout << "Hello World!";
+        cout << "Hello World!" << endl;
     }
 }
